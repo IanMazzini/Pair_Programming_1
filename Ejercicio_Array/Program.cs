92601b4e2babb847f8f48daa2981f2a0609eeebf
Ian Mazzini <ianmazzinibares@gmail.com>
2022-05-02 17:53:43 -0300

    Pair Programming - Day 1

---
+++ Ejercicio_Array/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Ejercicio_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[8];
+            int suma = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine("Escriba un numero");
+                if (Int32.TryParse(Console.ReadLine(), out array[i]))
+                {
+                    suma += array[i];
+                }
+                else{
+                    Console.WriteLine("El valor ingresado no es un numero");
+                }
+                
+            }
+            Console.WriteLine("La suma es: " + suma);
+            Console.WriteLine("El promedio es: " + (double) suma / array.Length);
+        }
+    }
+}
